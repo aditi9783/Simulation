020cd2d7ab668ca51c9c492c9c2a4f9973eeda99
Aditi Gupta <aditigupta@13-179-225.client.wireless.msu.edu>
2012-05-29 12:08:20 -0400

    Initial Commit

---
+++ cpp_code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp_code
+//
+//  Created by Aditi Gupta on 5/29/12.
+//  Copyright (c) 2012 Michigan State University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
